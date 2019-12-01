2b28b3f6de4986924196c1edb8540ab01f4cff62
rafael6 <rafael.gm01@gmail.com>
2019-12-01 12:54:45 -0500

    Removed some code

--- test.py
+++ test.py
@@ -4,12 +4,6 @@ import requests
 
 print(sys.version)
 print(sys.executable)
-print('hello world')
-
-
-
-name = input('what is your name?')
-print(name)
 
 r = requests.get('https://cnn.com')
 print(r.status_code)
